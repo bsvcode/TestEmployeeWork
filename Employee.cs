0fab3a14e52fc519e4cc2354b148615f21abcf90
bsvcode <86569669+bsvcode@users.noreply.github.com>
2021-06-29 23:33:52 +0300

    Create Employee.cs

---
+++ Employee.cs
@@ -0,0 +1,11 @@
+
+namespace TestEmployeeWork
+{
+  public class Employee
+  {
+      public int ID {get;set;}
+      public string FirstName ID {get;set;}
+      public string MiddletName ID {get;set;}
+      public string LasttName ID {get;set;}
+  }
+}
